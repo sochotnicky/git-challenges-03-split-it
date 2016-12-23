e8921869aa8df24af8a54f6766a1dddaa9f84609
Stanislav Ochotnicky <sochotnicky@redhat.com>
2016-12-23 12:34:26 +0100

    Add square methods

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ import sys
 def my_method():
     pass
 
+def square(num):
+    return num**2
+
 def my_second_method():
     print("""This will not work for a long time
             
             
     Or not""")
+
+def print_square():
+    num = int(raw_input("Input number: "))
+    print("Square: %d" % square(num))
+
 
